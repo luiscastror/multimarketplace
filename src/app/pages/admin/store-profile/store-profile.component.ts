959c3b7ad491c1c81e825287648263a76dba0a1a
Luis Castro R <ing.lcr.21@gmail.com>
2023-12-18 18:41:59 -0500

    resuelto

--- src/app/pages/admin/store-profile/store-profile.component.ts
+++ src/app/pages/admin/store-profile/store-profile.component.ts
@@ -12,43 +12,40 @@ export class StoreProfileComponent implements OnInit {
 
   form = new FormGroup({
     Id: new FormControl(''),
-    Descripcion: new FormControl(''),
     Biografia: new FormControl(''),
     CategoriaId: new FormControl(''),
+    Descripcion: new FormControl(''),
+    Logo: new FormControl(''),
+    Portada: new FormControl(''),
+    UsuarioId: new FormControl(''),
   });
 
   constructor(
     private MainService: MainService
-  ) {
-
-  }
+  ) { }
 
   ngOnInit(): void {
     this.loadStore();
     this.loadCategorias();
   }
 
-  loading4: boolean = true;
+  loading: boolean = true;
   store: any = {};
   loadStore() {
-    this.loading4 = true;
+    this.loading = true;
     this.MainService.ApiService.get('/admin/tiendas/' + this.MainService.AuthService.dataStore.Id).subscribe((resp: any) => {
-      this.form.patchValue(resp);
-      console.log(this.form.value);
-      console.log(resp)
       this.store = resp;
-      this.store.Id = this.MainService.AuthService.dataStore.Id;
-      this.store.UsuarioId = this.MainService.AuthService.dataUser.UsuarioId;
-      //this.store.CategoriaId = '91AD9E7D-6C7A-426A-90C8-0DC078E934A2';
-      this.loading4 = false;
+      this.form.patchValue(this.store);
+      this.form.patchValue({
+        UsuarioId: this.MainService.AuthService.dataUser.UsuarioId,
+        Id: this.MainService.AuthService.dataStore.Id
+      })
+      this.loading = false;
     })
   }
 
-
   change() {
     this.MainService.ApiService.put('/admin/tiendas/', this.form.value).subscribe((resp: any) => {
-      console.log('Este es el form:' + this.form.value)
-      console.log(resp)
       this.MainService.SnackbarService.show("Cambios guardados correctamente")
     })
   }
@@ -57,7 +54,6 @@ export class StoreProfileComponent implements OnInit {
   loadCategorias() {
     this.MainService.ApiService.get("/categorias").subscribe((resp: any) => {
       this.categorias = resp;
-      console.log(this.categorias)
     })
   }
 
