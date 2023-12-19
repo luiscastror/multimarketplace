200654e01c079382175a40b87daa6e077185f3cd
Orla08 <91040882+Orla08@users.noreply.github.com>
2023-12-19 12:10:19 -0500

    botones de categorias listos

--- src/app/pages/marketplace/store/store.component.ts
+++ src/app/pages/marketplace/store/store.component.ts
@@ -13,6 +13,8 @@ export class StoreComponent implements OnInit {
   id = this.ruta.snapshot.params.id;
   path_api_store: string = '/tiendas/' + this.id;
   path_api_store_items: string = '/productosXtienda/' + this.id;
+  todosLosProductos : boolean = true;
+  productForCategory : boolean = false;
 
   constructor(
     private MainService: MainService,
@@ -30,6 +32,7 @@ export class StoreComponent implements OnInit {
     this.loading = true;
     this.MainService.ApiService.get(this.path_api_store).subscribe((resp: any) => {
       this.info = resp;
+      //console.log(this.info)
     }, (error) => {
       console.error(error)
       this.MainService.SnackbarService.show(error.error.message);
@@ -41,15 +44,31 @@ export class StoreComponent implements OnInit {
 
   items: any = [];
   loading_items: boolean = true;
+  items2: any[] = [];
   load_store_items() {
     this.loading_items = true;
     this.MainService.ApiService.get(this.path_api_store_items).subscribe((resp: any) => {
       this.items = resp;
+      this.items2 = resp.Productos;
+      //console.log(this.items2);
     }, (error) => {
       console.error(error)
     }, () => {
       this.loading_items = false;
     })
+  }
+
+  itemsForSubCategory: any = [];
+  load_items_subcategory(subcategory:string){
+    this.itemsForSubCategory = this.items2.filter((product:any) => product.SubCategoria == subcategory)
+    this.todosLosProductos = false;
+    this.productForCategory = true;
+    console.log(this.itemsForSubCategory);
+  }
+
+  cargar(){
+    this.todosLosProductos = !this.todosLosProductos;
+    this.productForCategory = !this.productForCategory;
   }
 
 }
